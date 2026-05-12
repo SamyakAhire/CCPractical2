18b456166fdd6f72603b499e2a81d188550837e6
Samyak Ahire <104713752+SamyakAhire@users.noreply.github.com>
2026-05-12 17:19:55 +0530

    Add main class with user input and greeting

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class main()
+{
+  public void main(String args[])
+    {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your Name: ")
+    String name = sc.nextLine();
+    System.out.println("\nHello From "+ name + "Repo https://github.com/SamyakAhire/CCPractical2/main.java");
+    }
+}
